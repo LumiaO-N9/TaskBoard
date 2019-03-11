b0e14aa24f09c935b7b02d2f84f9243cbad6a2c1
LumiaO <1044740758@qq.com>
2019-03-11 15:40:38 +0800

    为数据库生成虚拟数据

---
+++ TaskBoard/fakes.py
@@ -0,0 +1,79 @@
+from TaskBoard.models import User, Project, Milestone, Task, Category
+from TaskBoard.extensions import db
+from faker import Faker
+from random import choice
+import random
+
+fake = Faker()
+
+
+def fake_admin_user():
+    user = User(
+        username='admin',
+        is_admin=True,
+        email=fake.email(),
+    )
+    user.set_password('admin')
+    db.session.add(user)
+    db.session.commit()
+
+
+def fake_Projects(count=3):
+    for i in range(count):
+        project = Project(
+            title=fake.word()
+        )
+        db.session.add(project)
+    db.session.commit()
+
+
+def fake_Users(count=10):
+    for i in range(count):
+        user = User(
+            username='worker%d' % i,
+            default_project_id=Project.query.get(random.randint(1, Project.query.count())).id,
+            email='10000%d@example.com' % i,
+        )
+        user.projects.append(Project.query.get(user.default_project_id))
+        user.projects.append((Project.query.get((user.default_project_id % Project.query.count()) + 1)))
+        user.set_password('TaskBoard')
+        db.session.add(user)
+    db.session.commit()
+
+
+def fake_milestones(count=5):
+    for i in range(Project.query.count()):  # 为每个project创建count-1或者count个Milestone
+        for j in range(random.randint(count - 1, count)):
+            milestone = Milestone(
+                title=fake.word(),
+                project=Project.query.get(i + 1)
+            )
+            db.session.add(milestone)
+    db.session.commit()
+
+
+def fake_categories(count=4):
+    for i in range(Project.query.count()):  # 为每个project创建count-2或者count个Category
+        for j in range(random.randint(count - 2, count)):
+            category = Category(
+                title=fake.name(),
+                project=Project.query.get(i + 1)
+            )
+            db.session.add(category)
+    db.session.commit()
+
+
+def fake_tasks(count=4):
+    for i in range(Milestone.query.count()):  # 为每个Milestone创建0~count个task
+        for j in range(random.randint(0, count)):
+            task = Task(
+                title=fake.name(),
+                description=fake.text(166),
+                due_date=fake.date_this_year(before_today=False, after_today=True),
+                points=random.randint(0, 10),
+                milestone=Milestone.query.get(i + 1)
+            )
+            task.category = choice(task.milestone.project.categories)
+            task.user = choice(task.milestone.project.users)
+            db.session.add(task)
+    db.session.commit()
