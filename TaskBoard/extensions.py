cd930b9218ebc7050dab7c70a6ef4b62c6167c75
LumiaO <1044740758@qq.com>
2019-03-14 13:09:50 +0800

    增加并初始化login_manager、csrf、bootstrap模块及login_manager的基础配置

--- TaskBoard/extensions.py
+++ TaskBoard/extensions.py
@@ -1,3 +1,20 @@
 from flask_sqlalchemy import SQLAlchemy
+from flask_login import LoginManager
+from flask_wtf import CSRFProtect
+from flask_bootstrap import Bootstrap
 
 db = SQLAlchemy()
+login_manager = LoginManager()
+csrf = CSRFProtect()
+bootstrap = Bootstrap()
+
+login_manager.login_view = 'auth.login'
+# login_manager.login_message = 'Your custom message'
+login_manager.login_message_category = 'warning'
+
+
+@login_manager.user_loader
+def load_user(user_id):
+    from TaskBoard.models import User
+    user = User.query.get(int(user_id))
+    return user
