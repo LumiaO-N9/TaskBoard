0715670b838f38f3c1ae41f21edcae03b20a2968
LumiaO <1044740758@qq.com>
2019-03-14 13:04:27 +0800

    login logout view function

--- TaskBoard/blueprints/auth.py
+++ TaskBoard/blueprints/auth.py
@@ -1,3 +1,40 @@
-from flask import Blueprint
+from flask import Blueprint, redirect, url_for, flash, render_template
+from flask_login import current_user, login_user, logout_user, login_required
+from TaskBoard.forms import LoginForm
+from TaskBoard.models import User
+from TaskBoard.utils import redirect_back
+import datetime
 
 auth_bp = Blueprint('auth', __name__)
+
+
+@auth_bp.route('/login', methods=['GET', 'POST'])
+def login():
+    if current_user.is_authenticated:
+        return redirect(url_for('taskboard.index'))
+
+    form = LoginForm()
+    if form.validate_on_submit():
+        username = form.username.data
+        password = form.password.data
+        remember = form.remember.data
+        print(username, password, remember)
+        user = User.query.filter_by(username=username).first()
+        print(user)
+        if user:
+            if user.validate_password(password):
+                duration = datetime.timedelta(days=7)
+                login_user(user, remember, duration=duration)
+                flash('Welcome back.', 'info')
+                return redirect_back()
+            flash('Invalid username or password.', 'warning')
+        else:
+            flash('No account.', 'danger')
+    return render_template('auth/login.html', form=form)
+
+
+@auth_bp.route('/logout')
+def logout():
+    logout_user()
+    flash('Logout success.', 'info')
+    return redirect(url_for('auth.login'))
