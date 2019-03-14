2f960aac8809d65649e0aed1fb4eabdd66fe5930
LumiaO <1044740758@qq.com>
2019-03-14 13:05:46 +0800

    board 界面的视图函数 2019/3/13

--- TaskBoard/blueprints/taskboard.py
+++ TaskBoard/blueprints/taskboard.py
@@ -1,3 +1,10 @@
-from flask import Blueprint
+from flask import Blueprint, render_template
+from flask_login import login_required
 
 taskboard_bp = Blueprint('taskboard', __name__)
+
+
+@taskboard_bp.route('/')
+@login_required
+def index():
+    return render_template('taskboard/board.html')
