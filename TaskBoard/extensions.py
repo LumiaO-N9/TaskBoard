c89ed48c0e9b55b2b48d0811998b3937fd506141
LumiaO <1044740758@qq.com>
2019-04-23 10:11:06 +0800

    uninstall bootstrap-flask

--- TaskBoard/extensions.py
+++ TaskBoard/extensions.py
@@ -1,15 +1,14 @@
 from flask_sqlalchemy import SQLAlchemy
 from flask_login import LoginManager
 from flask_wtf import CSRFProtect
-from flask_bootstrap import Bootstrap
-from flask_moment import  Moment
+from flask_moment import Moment
+from flask_debugtoolbar import DebugToolbarExtension
 
 db = SQLAlchemy()
 login_manager = LoginManager()
 csrf = CSRFProtect()
-bootstrap = Bootstrap()
 moment = Moment()
-
+toolbar = DebugToolbarExtension()
 login_manager.login_view = 'auth.login'
 # login_manager.login_message = 'Your custom message'
 login_manager.login_message_category = 'warning'
