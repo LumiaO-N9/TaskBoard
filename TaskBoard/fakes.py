dd7aec33717672caf2fd7eaa49269d32eb06af1b
LumiaO <1044740758@qq.com>
2019-05-08 14:51:42 +0800

    faking fake logs as the same time with faking other models

--- TaskBoard/fakes.py
+++ TaskBoard/fakes.py
@@ -1,4 +1,4 @@
-from TaskBoard.models import User, Project, Milestone, Task, Category, Comment, Tag
+from TaskBoard.models import User, Project, Milestone, Task, Category, Comment, Tag, Log
 from TaskBoard.extensions import db
 from faker import Faker
 from random import choice
@@ -15,9 +15,13 @@ color_array = ['#dff0d8', '#d9edf7', '#fcf8e3', '#f2dede']
 def fake_Projects(count=3):
     for i in range(count):
         project = Project(
-            title=fake.word().title()
+            title=fake.word().title(),
+            create_time=fake.date_time_between(start_date="-37d", end_date="-35d", tzinfo=None)
         )
+        log = Log(log='Create Project ' + project.title)
+        log.create_time = project.create_time
         db.session.add(project)
+        db.session.add(log)
     db.session.commit()
 
 
@@ -26,16 +30,25 @@ def fake_admin_user():
         username='admin',
         is_admin=True,
         email=fake.email(),
+        create_time=fake.date_time_between(start_date="-35d", end_date="-35d", tzinfo=None)
     )
     user.set_password('TaskBoard.')
+    log = Log(log='Create Admin User')
+    log.create_time = user.create_time
     db.session.add(user)
+    db.session.add(log)
     db.session.commit()
 
 
 def fake_Tags(count=8):
     for i in range(count):
-        tag = Tag(tag=fake.word().title())
+        tag = Tag(tag=fake.word().title(),
+                  create_time=fake.date_time_between(start_date="-34d", end_date="-34d", tzinfo=None)
+                  )
+        log = Log(log='Create Tag: ' + tag.tag)
+        log.create_time = tag.create_time
         db.session.add(tag)
+        db.session.add(log)
     db.session.commit()
 
 
@@ -46,6 +59,7 @@ def fake_Users(count=10):
             default_project=Project.query.get(random.randint(1, Project.query.count())),
             access_project=Project.query.get(random.randint(1, Project.query.count())),
             email='10000%d@example.com' % i,
+            create_time=fake.date_time_between(start_date="-34d", end_date="now", tzinfo=None)
         )
         user.tag_id = random.randint(1, Tag.query.count())
         user.set_password('TaskBoard.')
@@ -54,6 +68,9 @@ def fake_Users(count=10):
         else:
             user.projects.append(user.default_project)
             user.projects.append(user.access_project)
+        log = Log(log='Create User ' + user.username)
+        log.create_time = user.create_time
+        db.session.add(log)
         db.session.add(user)
     db.session.commit()
 
@@ -65,9 +82,13 @@ def fake_milestones(count=5):
             milestone = Milestone(
                 title=fake.word().title(),
                 order=j,
-                project=Project.query.get(i + 1)
+                project=Project.query.get(i + 1),
+                create_time=fake.date_time_between(start_date="-35d", end_date="-30d", tzinfo=None)
             )
+            log = Log(log='Create Milestone ' + milestone.title + ' in Project ' + milestone.project.title)
+            log.create_time = milestone.create_time
             db.session.add(milestone)
+            db.session.add(log)
     db.session.commit()
 
 
@@ -78,8 +99,12 @@ def fake_categories(count=4):
             category = Category(
                 title=fake.name(),
                 project=Project.query.get(i + 1),
-                color=choice(color_array)
+                color=choice(color_array),
+                create_time=fake.date_time_between(start_date="-35d", end_date="-30d", tzinfo=None)
             )
+            log = Log(log='Create Category ' + category.title + ' for Project ' + category.project.title)
+            log.create_time = category.create_time
+            db.session.add(log)
             db.session.add(category)
     db.session.commit()
 
@@ -101,6 +126,9 @@ def fake_tasks(count=4):
             task.is_complete = choice([False, True, False])
             task.category = choice(task.milestone.project.categories)
             task.user = choice(task.milestone.project.users)
+            log = Log(log='Create Task ' + task.title + ' in Milestone ' + task.milestone.title)
+            log.create_time = task.create_time
+            db.session.add(log)
             db.session.add(task)
     db.session.commit()
 
@@ -111,8 +139,13 @@ def fake_comments(count=3):
         for j in range(count):  # generate three comments for every task
             comment = Comment(
                 text=fake.text(100),
-                task_id=Task.query.get(i + 1).id
+                task=Task.query.get(i + 1)
             )
             comment.user_id = choice(User.query.all()).id
+            comment.create_time = fake.date_time_between(start_date=comment.task.create_time, end_date="now",
+                                                         tzinfo=None)
+            log = Log(log='Create Comment for Task ' + comment.task.title)
+            log.create_time = comment.create_time
+            db.session.add(log)
             db.session.add(comment)
     db.session.commit()
