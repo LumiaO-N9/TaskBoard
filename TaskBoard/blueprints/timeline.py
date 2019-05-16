20787420e1dd3419776523d99b5d93224d64a525
LumiaO <1044740758@qq.com>
2019-05-16 22:11:56 +0800

    fix bug

--- TaskBoard/blueprints/timeline.py
+++ TaskBoard/blueprints/timeline.py
@@ -19,14 +19,18 @@ def index(week):
     last_log = Log.query.order_by(Log.create_time).first()
     week_logs_dict = collections.OrderedDict()
     last_week = week + 1
-    for i in range(7):
-        date_tmp = today - timedelta(days=i)
-        if datetime.combine(date_tmp, datetime.min.time()) < last_log.create_time:
-            last_week = -1
-        start = datetime(date_tmp.year, date_tmp.month, date_tmp.day, 0)
-        end = datetime(date_tmp.year, date_tmp.month, date_tmp.day, 23)
-        logs = Log.query.filter(Log.create_time >= start).filter(Log.create_time <= end).order_by(
-            Log.create_time.desc()).all()
-        week_logs_dict[start.strftime('%m月%d日')] = logs
+    if not last_log:
+        last_week = -1
+        week = 0
+    else:
+        for i in range(7):
+            date_tmp = today - timedelta(days=i)
+            if datetime.combine(date_tmp, datetime.min.time()) < last_log.create_time:
+                last_week = -1
+            start = datetime(date_tmp.year, date_tmp.month, date_tmp.day, 0, 0, 0)
+            end = datetime(date_tmp.year, date_tmp.month, date_tmp.day, 23, 59, 50)
+            logs = Log.query.filter(Log.create_time >= start).filter(Log.create_time <= end).order_by(
+                Log.create_time.desc()).all()
+            week_logs_dict[start.strftime('%m月%d日')] = logs
     return render_template('timeline/timeline.html', week_logs_dict=week_logs_dict, last_week=last_week,
                            before_week=week - 1)
