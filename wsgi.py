18768b16c362a35761a53c4c6155d88b0c87fe80
LumiaO <1044740758@qq.com>
2019-05-27 21:42:13 +0800

    edit wsgi.py for socketio

--- wsgi.py
+++ wsgi.py
@@ -1,8 +1,11 @@
 from TaskBoard import create_app
 from dotenv import load_dotenv
+from TaskBoard.extensions import socketio
 import os
 
 dotenv_path = os.path.join(os.path.dirname(__file__), '.env')
 if os.path.exists(dotenv_path):
     load_dotenv(dotenv_path)
 app = create_app('production')
+if __name__ == '__main__':
+    socketio.run(app)
